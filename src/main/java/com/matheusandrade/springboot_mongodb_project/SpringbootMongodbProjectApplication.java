1f1737fac643a3b549fdb51b6fe1ef27e8c1aaee
MatheusMOAndrade <andradematheusmo@gmail.com>
2024-09-23 13:18:54 -0300

    first commit: project created

---
+++ src/main/java/com/matheusandrade/springboot_mongodb_project/SpringbootMongodbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.matheusandrade.springboot_mongodb_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbProjectApplication.class, args);
+	}
+
+}
